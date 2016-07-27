308c707bc2a453c6597b76032d0ad3605fba9ef9
prentis <szlun@qq.com>
2016-07-27 23:07:52 +0800

    test

---
+++ src/net/test/Main.java
@@ -0,0 +1,10 @@
+package net.test;
+
+import net.utils.DateUtil;
+
+public class Main {
+	public static void main(String[] args) {
+		long time = DateUtil.getTime();
+		System.out.println(time);
+	}
+}
